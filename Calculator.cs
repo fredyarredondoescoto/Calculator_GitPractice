20aca8c4a5dc66d458020783e233897ec829b2c5
Guillermo Ruvalcaba <guillermo8931@gmail.com>
2020-05-08 09:31:10 -0500

    Suma

--- Calculator.cs
+++ Calculator.cs
@@ -10,6 +10,19 @@ namespace GitPractice_I
     {
         static void Main(string[] args)
         {
+            Suma();
+            int num1, num2, resultado;
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            resultado = num1 + num2;
+
+            Console.WriteLine("El resultado es " + resultado);
+
+            Console.Read();
+
+
         }
     }
 }
